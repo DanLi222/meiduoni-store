d610433669aa8b9c94960618e78d10d0d3184e98
DanLi222 <60045359+DanLi222@users.noreply.github.com>
2020-11-20 20:56:34 -0500

    Added product show page (#3)
    Co-authored-by: Dan <>

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,10 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
@@ -36,7 +40,6 @@ class ProductsController < ApplicationController
 
     redirect_to products_path
   end
-
 
   private
     def product_params
