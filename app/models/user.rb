f425cdfeb5f7ad3de83334c17aae148f4086897f
Dan <>
2020-11-30 11:47:58 -0500

    Setup cart model Setup relationship between cart and user Added after create in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  has_many :carts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :create_carts
+
+  private
+  def create_carts
+    self.carts << Cart.create()
+  end
 end
