2cb8874b3dbddb897e30e6c1a1ff10390804f4bf
Dan <>
2020-12-02 14:58:38 -0500

    Updated size dropdown Added disable on some options Linked inventory to selected product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,16 +5,32 @@ class ProductsController < ApplicationController
 
   def show
     @products = Product.where(sku: params[:id])
+    @disabled_inventories = []
     if params[:product_id].nil?
       @selected_product = @products.first
+      @inventory_array = @selected_product.inventories.map { |inventory| [inventory.size, inventory.id, inventory.quantity]}
+      @disabled_inventories = disabled_inventories(@inventory_array)
     else
       @selected_product = Product.find(params[:product_id])
+      @inventory_array = @selected_product.inventories.map { |inventory| [inventory.size, inventory.id, inventory.quantity]}
+      @disabled_inventories = disabled_inventories(@inventory_array)
       render partial: "product_detail"
     end
+
   end
   
   private
     def product_params
       params.require(:product).permit(:sku, :color, :image)
+    end
+
+    def disabled_inventories(inventory_array)
+      disabled_inventories = []
+      inventory_array.each do |inventory|
+        if inventory[2] == 0
+          disabled_inventories << inventory
+        end
+      end
+      disabled_inventories
     end
 end
