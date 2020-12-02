7e92cc6f5b477e65133b9a9d800f1dee7b716d95
Dan <>
2020-12-02 13:22:42 -0500

    Updated color selection function Update partial view according to color Created partial Added script to partial refresh

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,17 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    @products = Product.where(sku: params[:id])
+    if params[:product_id].nil?
+      @selected_product = @products.first
+    else
+      @selected_product = Product.find(params[:product_id])
+      render partial: "product_detail"
+    end
   end
   
   private
     def product_params
       params.require(:product).permit(:sku, :color, :image)
     end
-
 end
