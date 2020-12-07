d2a4693283b25790e1b4ffd37443f34b8e38669b
Dan <>
2020-12-07 15:33:01 -0500

    Updated total in cart show page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,5 +4,12 @@ class CartsController < ApplicationController
     @line_items_array = @cart.line_items.map {
         |line_item| [line_item.inventory.product.image, line_item.inventory.product.sku, line_item.inventory.product.property.price, line_item.quantity, line_item.inventory.size]
     }
+
+    @subtotal = BigDecimal("0")
+    @line_items_array.each do |line_item|
+      @subtotal += line_item[3] * line_item[2]
+    end
+    binding.pry
+    @total = @subtotal * BigDecimal("1.13")
   end
 end
