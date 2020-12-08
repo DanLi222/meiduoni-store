88fb55f7a28ef0f89f6e95fe611e818cde862891
Dan <>
2020-12-08 16:36:30 -0500

    Linked line_item to current cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,9 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :line_items
+  scope :user_carts, -> (user) { where(user_id: user.id) }
+
+  def self.current_cart(user)
+    user_carts(user).last
+  end
 end
