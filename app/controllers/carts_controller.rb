bbf3261090710acf547b0d16ab00397a1e2041a1
Dan <>
2020-12-08 15:48:44 -0500

    Cleaned code for cart views

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,19 @@
 class CartsController < ApplicationController
   def show
     @cart = current_user.carts.last
-    @line_items_array = @cart.line_items.map {
-        |line_item| [line_item.inventory.product.image, line_item.inventory.product.sku, line_item.inventory.product.property.price, line_item.quantity, line_item.inventory.size]
-    }
+    @line_items_array = @cart.line_items.map do |line_item|
+      {
+        image: line_item.inventory.product.image,
+        sku: line_item.inventory.product.sku,
+        price: line_item.inventory.product.property.price,
+        quantity: line_item.quantity,
+        size: line_item.inventory.size
+      }
+    end
 
     @subtotal = BigDecimal("0")
     @line_items_array.each do |line_item|
-      @subtotal += line_item[3] * line_item[2]
+      @subtotal += line_item[:price] * line_item[:quantity]
     end
     @total = @subtotal * BigDecimal("1.13")
   end
