47924a42c582f8b74541afa7064d98b8cf5284c0
Dan <>
2020-12-09 16:05:35 -0500

    Updated order information on checkout page

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,7 @@
 class AddressesController < ApplicationController
+  def new
+    @current_cart = Cart.current_cart(current_user)
+    @line_items = @current_cart.line_items
+    @total = @line_items.map{|item| item.inventory.product.property.price * item.quantity }.sum
+  end
 end
