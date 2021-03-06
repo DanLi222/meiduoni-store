492c93beabff8fde79d2eb40a1999b8328dd95f0
DanLi222 <60045359+DanLi222@users.noreply.github.com>
2021-03-06 14:36:24 -0500

    Update cart controller (#55)
    * Updated cart controller
    Updated line item model
    
    Co-authored-by: Dan <>

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,21 +1,6 @@
 class CartsController < ApplicationController
   def show
     @cart = Cart.current_cart(current_user)
-    @line_items_array = @cart.line_items.map do |line_item|
-      {
-        id: line_item.id,
-        image: line_item.inventory.product.image,
-        sku: line_item.inventory.product.sku,
-        price: line_item.inventory.product.property.price,
-        quantity: line_item.quantity,
-        size: line_item.inventory.size
-      }
-    end
-
-    @subtotal = BigDecimal("0")
-    @line_items_array.each do |line_item|
-      @subtotal += line_item[:price] * line_item[:quantity]
-    end
-    @cart.update(total: @subtotal * BigDecimal("1.13"))
+    @line_items = @cart.line_items
   end
 end
