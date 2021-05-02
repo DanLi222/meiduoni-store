86aa4a338e561729fc6ce7eefd3211204ef09f6c
Dan <>
2021-05-02 14:31:42 -0400

    Updated language switch button and url Updated routes Updated checkout route in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  get 'activity/mine'
-  get 'activity/feed'
   root to: 'products#index'
-  resources :products do
-    resources :properties
-    resources :inventories
-  end
-  namespace :admins do
-    resources :products
-    resources :properties
-    resources :inventories
-  end
+  
+  scope "/:locale" do
+    devise_for :users, :controllers => {:registrations => "registrations"}
 
-  resources :carts
-  resources :addresses
+    resources :products do
+      resources :properties
+      resources :inventories
+    end
 
-  get 'add_to_cart' => 'line_items#add_to_cart'
-  get 'checkout' => 'checkout#checkout'
-  post 'checkout' => 'checkout#checkout'
-  get 'update_billing_address' => 'checkout#update_billing_address'
-  post 'set_locale' => 'application#set_locale'
+    namespace :admins do
+      resources :products
+      resources :properties
+      resources :inventories
+    end
+
+    resources :carts
+    resources :addresses
+
+    get 'add_to_cart' => 'line_items#add_to_cart'
+    get 'checkout' => 'checkout#checkout'
+    post 'checkout' => 'checkout#checkout'
+    get 'update_billing_address' => 'checkout#update_billing_address'
+    post 'set_locale' => 'application#set_locale'
+  end
 end
